9b99039fe049083962944d1407de54e08823a681
unknown <2amod@e041edu.local>
2026-09-09 17:29:29 -0300

    Nova Atividade JavaScript

---
+++ javascript/03-Lab/Intervalo_datas.js/sript.js
@@ -0,0 +1,7 @@
+function calculadora(){
+    var dia = parseInt(document.getElementById("data_nasc").value);
+    var mes = parseInt(document.getElementById("data_mes").value);
+    var ano = parseInt(document.getElementById("data_ano").value);
+
+}
+
